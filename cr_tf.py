d95ae7a7a781531cc700ace66af9ba484c086e3d
Jun Jo <j.jo@kist-europe.de>
2017-03-08 11:04:13 +0100

    add LSTM layer

--- cr_tf.py
+++ cr_tf.py
@@ -9,7 +9,7 @@ from reader import Reader
 
 class FullConnected(object):
   '''
-  Restricted Boltzmann Machine
+  Restricted Boltzmann Machine layer
   Args:
     x: input tensor array
     shape: [input size, output size]
@@ -24,8 +24,25 @@ class FullConnected(object):
     return tf.add(tf.matmul(self.x, self.w), self.b)
 
 class LSTM(object):
-  def __init__(self,input, shape):
-    pass
+  '''
+  Long Short Term Memory layer
+  Args:
+    x: input tensor array shape = [LSTM step number, currency number]
+    shape: [number of hidden layer, LSTM hidden cell size]
+  Return:
+    outputs, state values
+  '''
+  def __init__(self, x, shape):
+    self.x = x
+    lstm_cell = tf.contrib.rnn.LSTMCell(shape[1], forget_bias=1.0)
+    cell = tf.contrib.rnn.MultiRNNCell([lstm_cell for _ in range(shape[0])])
+    self.initial_state = cell.zero_state(1, tf.float32) # initial states
+    x = tf.expand_dims(x, 0)  # demension for input batch
+    x = tf.unstack(x, axis=1) # make sequence of input data (length = LSTM step number)
+    self.outputs, self.state = tf.contrib.rnn.static_rnn(cell, x, initial_state=self.initial_state)
+
+  def output(self):
+    return self.outputs, self.state
 
 class BNormal(object):
   '''
@@ -42,7 +59,7 @@ class BNormal(object):
     self.beta = tf.Variable(tf.constant(0.0, shape=shape), name='beta', trainable=True)
     self.gamma = tf.Variable(tf.constant(1.0, shape=shape), name='gamma', trainable=True)
   def output(self):
-    batch_mean, batch_var = tf.nn.moments(self.x, [0], name='moments')
+    batch_mean, batch_var = tf.nn.moments(self.x, [0])
     ema = tf.train.ExponentialMovingAverage(decay=0.5)
     def mean_var_with_update():
       ema_apply_op = ema.apply([batch_mean, batch_var])
@@ -51,15 +68,15 @@ class BNormal(object):
     mean, var = tf.cond(self.train,
                         mean_var_with_update,
                         lambda: (ema.average(batch_mean), ema.average(batch_var)))
-    return tf.nn.batch_normalization(self.x, mean, var, self.beta, self.gamma, 1e-3)
+    return tf.nn.batch_normalization(self.x, mean, var, self.beta, self.gamma, 1e-5)
  
 def main(_):
-  MAX_STEPS = 20000
-  BATCH_SIZE = 30
+  MAX_STEPS = 2000
+  BATCH_SIZE = 5
   LEARNING_RATE = 0.005 # learning rate for optimizer
   KEEP_RATE = 1.0       # keep rate for dropout
-  
-  train_phase = tf.placeholder(tf.bool, name='phase_train')
+  train_phase = tf.placeholder(tf.bool, name='train_phase')
+
   with tf.device('/cpu:0'):
     ### load data from TFRecords file
     index, tfrecords = Reader().index, Reader().read_and_decode('input.tfrecords')
@@ -72,35 +89,38 @@ def main(_):
  
   with tf.device('/gpu:0'):
     ### define neural network form
-    with tf.variable_scope('hidden1'):
-      h1 = FullConnected(data_batch, [8, 100]).output()
-      h1_br = BNormal(h1, [100], train_phase).output()
-      h1_relu = tf.nn.relu(h1_br)
-      h1_out = tf.nn.dropout(h1_relu, KEEP_RATE)
+    with tf.variable_scope('lstm'):
+      lstm_out, state = LSTM(data_batch, [2, 50]).output()
+      lstm_out = tf.squeeze(lstm_out)
+#    with tf.variable_scope('hidden1'):
+#      h1 = FullConnected(lstm_out, [50, 50]).output()
+#      h1_br = BNormal(h1, [50], train_phase).output()
+#      h1_out = tf.nn.relu(h1_br)
+#      h1_out = tf.nn.dropout(h1_out, KEEP_RATE)
 
-    with tf.variable_scope('hidden2'):
-      h2 = FullConnected(h1_out, [100, 50]).output()
-      h2_br = BNormal(h2, [50], train_phase).output()
-      h2_relu = tf.nn.relu(h2_br)
-      h2_out = tf.nn.dropout(h2_relu, KEEP_RATE)
+#    with tf.variable_scope('hidden2'):
+#      h2 = FullConnected(h1_out, [100, 50]).output()
+#      h2_br = BNormal(h2, [50], train_phase).output()
+#      h2_out = tf.nn.relu(h2_br)
+#      h2_out = tf.nn.dropout(h2_out, KEEP_RATE)
 
-    with tf.variable_scope('hidden3'):
-      h3 = FullConnected(h2_out, [50, 50]).output()
-      h3_br = BNormal(h3, [50], train_phase).output()
-      h3_relu = tf.nn.relu(h3_br)
-      h3_out = tf.nn.dropout(h3_relu, KEEP_RATE)
+#    with tf.variable_scope('hidden3'):
+#      h3 = FullConnected(h2_out, [50, 50]).output()
+#      h3_br = BNormal(h3, [50], train_phase).output()
+#      h3_out = tf.nn.relu(h3_br)
+#      h3_out = tf.nn.dropout(h3_out, KEEP_RATE)
 
     with tf.variable_scope('output'):
-      out = FullConnected(h3_out, [50, 2]).output()
+      out = FullConnected(lstm_out, [50, 2]).output()
 
     ### define cost function and optimizer
   with tf.device('/cpu:0'):
-    global_step = tf.Variable(0, trainable=False)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
   with tf.device('/gpu:0'):
     decayed_lr = tf.train.exponential_decay(LEARNING_RATE, global_step, int(MAX_STEPS/3), 0.95, staircase=True)
     entropy = tf.nn.softmax_cross_entropy_with_logits(labels=label_batch, logits=out) 
     loss_op = tf.reduce_mean(entropy)
-    opt = tf.train.AdamOptimizer(decayed_lr).minimize(loss_op, global_step=global_step)
+    opt = tf.train.GradientDescentOptimizer(decayed_lr).minimize(loss_op, global_step=global_step)
 
   with tf.device('/cpu:0'):
     ### define test values
@@ -112,16 +132,23 @@ def main(_):
   sess = tf.Session(config=config)
   sess.run(tf.global_variables_initializer())
 
+  print("--- Trainable Variables ---")
+  for val in tf.trainable_variables():
+    print(tf.Graph.is_feedable(sess.graph, val), val)
+  print("---------------------------")
+
   tf.train.start_queue_runners(sess=sess)
 
   ### train model
   for step in range(MAX_STEPS):
     _, loss, data_p = sess.run([opt, loss_op, data_batch], {train_phase:True})
-    if (step+1) % 1000 == 0:
+    if (step+1) % 100 == 0:
       acc_p = accuracy.eval({train_phase:False}, session=sess)
       print("[{:5d}/{:5d}] loss:{:.3f}, train accuracy:{:.3f}".format(step+1, MAX_STEPS, loss, acc_p))
   print("--- Training Finished ---")
 
+  writer = tf.summary.FileWriter('./graph', sess.graph)
+  writer.close()
   ### test model
 #  print("Test  Accuracy: {:.3f}".format(sess.run(accuracy, feed_dict={x:test_x, y:test_y})))
 
